d57d2ff7ba4c977189eb8b955329ea0d3be9749d
MarcinZiabek <marcin@ziabek.com>
2023-05-04 20:27:55 +0200

    Upgraded to version 2023.4

--- Program.cs
+++ Program.cs
@@ -3,27 +3,31 @@ using System.Diagnostics;
 using System.IO;
 using System.Runtime.InteropServices;
 using QuestPDF.Fluent;
+using QuestPDF.Infrastructure;
 using QuestPDF.Previewer;
 
 namespace QuestPDF.ExampleInvoice
 {
     class Program
     {
-        /// <summary>
-        /// For documentation and implementation details, please visit:
-        /// https://www.questpdf.com/documentation/getting-started.html
-        /// </summary>
         static void Main(string[] args)
         {
+            // Please make sure that you are eligible to use the Community license.
+            // To learn more about the QuestPDF licensing, please visit:
+            // https://www.questpdf.com/pricing.html
+            Settings.License = LicenseType.Community;
+            
+            // For documentation and implementation details, please visit:
+            // https://www.questpdf.com/documentation/getting-started.html
             var model = InvoiceDocumentDataSource.GetInvoiceDetails();
             var document = new InvoiceDocument(model);
 
             // Generate PDF file and show it in the default viewer
-            GenerateDocumentAndShow(document);
+            //GenerateDocumentAndShow(document);
             
             // Or open the QuestPDF Previewer and experiment with the document's design
             // in real-time without recompilation after each code change
-            //document.ShowInPreviewer();
+            document.ShowInPreviewer();
         }
 
         static void GenerateDocumentAndShow(InvoiceDocument document)
